964044c1f34f5cbf64425fd7e7deca8d12f440db
Jonny K <Kempj2.jk@gmail.com>
2020-11-12 15:11:57 -0800

    seeding file correctly. It calls the load file from where the script is run from, so the root level

--- fixtures/seedDB.js
+++ fixtures/seedDB.js
@@ -1,8 +1,7 @@
 const sequelize_fixtures = require('sequelize-fixtures');
-const models = require('./models');
+const models = require('../models');
+
 console.log("Seeding data---------------------------------------------")
-const seed = sequelize_fixtures.loadFile("./seeds.json", models).then(function(){
+const seed = sequelize_fixtures.loadFile('./fixtures/seeds.json', models).then(function(){
     console.log("Seeding successful")
 });
-
-seed();
